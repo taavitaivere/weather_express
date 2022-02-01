5784a04a06eb2cce5ce7ce139b192b4835413e0b
Taavi taivere <taavi.taivere@khk.ee>
2022-02-01 14:43:00 +0200

    Created API error handling

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const getWeatherDataPromise = (url) => {
                 let result = {
                     description: description,
                     city: city,
-                    temp: temp
+                    temp: temp,
+                    error: null
             }
             resolve(result)
     })
@@ -45,6 +46,9 @@ app.all('/', function (req, res) {
     getWeatherDataPromise(url)
         .then(data => {
             res.render('index', data)
+        })
+        .catch(error => {
+            res.render('index', {error:'Problem with getting data, try again'})
         })
 })
 app.listen(3000, ()=> {
