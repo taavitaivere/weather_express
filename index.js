29ac56db4d2da833cdf1ba29601021d5c424f0ec
Taavi taivere <taavi.taivere@khk.ee>
2022-02-01 13:26:25 +0200

    Created project template files

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.set('views engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', function (req, res) {
-    res.render('index');
-});
+    res.render('index')
+})
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
-});
+})
