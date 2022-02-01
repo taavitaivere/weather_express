66c49ecdca694c68fa762f936b7f175fe50418d8
Taavi taivere <taavi.taivere@khk.ee>
2022-02-01 14:31:47 +0200

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -2,33 +2,50 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch =require('node-fetch')
-
+const bodyParser = require('body-parser')
+const {response} = require("express");
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const key = "77f97b598769bf8861004c74e30ebbba"
-let city = 'Tartu'
-
-app.get('/', function (req, res) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-        .then((response) => {
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
             return response.json()
         })
-        .then((data)=> {
-            let description = data.weather[0].description
-            let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp)-273.15)
-            res.render('index', {
-                description: description,
-                city: city,
-                temp: temp
+            .then(data => {
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp)-273.15)
+                let result = {
+                    description: description,
+                    city: city,
+                    temp: temp
+            }
+            resolve(result)
+    })
+            .catch(error => {
+            reject(error)
             })
+    })
+}
+
+app.all('/', function (req, res) {
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    }
+    if(req.method == 'POST'){
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data => {
+            res.render('index', data)
         })
-})
-app.post ('/', function (req,res) {
-    console.log('req.body')
-    res.redirect('/')
 })
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
